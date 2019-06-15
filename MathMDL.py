c7ba9bb34ba74536b3e13c805290bc67c98b9b5d
Nilay Arangil <50153327+nilayarangil@users.noreply.github.com>
2019-06-15 13:27:30 -0500

    Math Module initial check in

---
+++ MathMDL.py
@@ -0,0 +1,122 @@
+import math
+import statistics
+
+# This class only does basic math operations on a list of numbers
+class MathMDL:
+    def __init__(self):
+        self.numbers = []
+     # Method adds the list of numbers passed   
+    def Add(self):
+        c1 = 0
+        for n in self.numbers:
+            c1 = c1 + float(n) 
+        return c1
+    # Method multiplies all the numbers in the list 
+    def Multiply(self):
+        c1 = 1
+        for n in self.numbers:
+            c1 = c1 * float(n)
+        return c1
+    # Method substracts the first two numbers in the list passed
+    def Subtract(self):
+        if len(self.numbers) > 2 or len(self.numbers) <= 1:
+            print("Error calculating difference. Please pass only two numbers.") 
+        elif len(self.numbers) == 2:
+            return float(self.numbers[0])-float(self.numbers[1])        
+    # Method divides the first two numbers in the list
+    def Divide(self):
+        if len(self.numbers) > 2 or len(self.numbers) <= 1:
+            print("Error dividing. Please pass only two numbers.") 
+        elif len(self.numbers) == 2:
+             return float(self.numbers[0])/float(self.numbers[1])
+    def GetRange(self):
+        return float(max(self.numbers))-float(min(self.numbers))
+    def GetAverage(self):   
+         s = self.Add()
+         return s/len(self.numbers)  
+    def GetMode(self):
+        return statistics.mode(self.numbers)
+    def GetMode2(self):
+        count = 0
+        mode= 0
+        for n in self.numbers:
+            if self.numbers.count(n) > count:
+                count = self.numbers.count(n)
+                mode = n
+        return mode
+
+
+    
+
+
+# This class defines a cricle and attributes and methods of a circle
+class Circle:
+    def __init__(self):
+        self.radius = 0
+    def GetArea(self):
+        return math.pi * self.radius * self.radius
+        
+# Define class for rectangle and have square class inherit from it.
+class Rectangle:
+    def __init__(self):
+        self.length = 0
+        self.width = 0
+    def GetArea(self):
+        return self.length * self.width
+    def GetPerimeter(self):
+        return (self.length + self.width) * 2
+ 
+class Square(Rectangle):
+    def __init__(self):
+        Rectangle.__init__(self)
+    def GetSquareArea(self):
+        return self.length * self.length
+    def GetSquarePerimeter(self):
+        return self.length *4
+
+       
+# Define class for Cylinder. it should have one method GetVolume
+class Cylinder(Circle):
+    def __init__(self):
+        Circle.__init__(self)
+        self.height = 0      
+        
+    def GetCylinderVolume(self):
+        return math.pi * self.radius * self.radius * self.height
+    def GetCylinderSurfaceArea(self):
+        return (math.pi * self.radius * self.radius * 2)+((math.pi * (self.radius *2)) * self.height) 
+    
+# Define triangle. It should have method GetArea,Perimeter
+class Triangle():
+    def __init__(self):
+        self.side1 = 0
+        self.side2 = 0
+        self.side3 = 0
+        self.base = 0
+        self.height = 0 
+    
+    def GetPerimeter(self):
+        return self.side1 + self.side2 + self.side3
+    def GetArea(self):
+        return self.base * self.height / 2 
+# Define Temperture class. attributes should be celcius,farenheat. Methods mustbe ConvertToCelcius
+# and ConvertToFarenheats
+class Temperature:
+    def __init__(self):
+        self.farenheit = 0
+        self.celcius = 0
+    def FarenheitToCelcius(self):
+        return (self.farenheit-32)*(5/9)
+    def CelciusToFarenheit(self):
+        return ((self.celcius * 9/5) + 32)
+class Pyramid:
+    def __init__(self):
+        self.length = 0
+        self.width = 0
+        self.height = 0
+    def GetPyramidArea(self):
+        return (self.length*self.width*self.height)/3
+
+
+        
+        
---
+++ arunstest.py
@@ -0,0 +1,56 @@
+import MathMDL as M
+'''
+mathmdl = MathMDL.MathMDL()
+mathmdl.numbers = [5,2,1]
+print("the sum result is", mathmdl.Add())
+print("difference is",mathmdl.Subtract())
+
+circle = MathMDL.Circle()
+circle.radius =2
+print("area of the circle is", circle.GetArea())
+
+rect = MathMDL.Rectangle()
+rect.length = 3.4
+rect.width = 2
+print("area of this rectangle is",rect.GetArea())
+print("perimeter of this rectangle is",rect.GetPerimeter())
+
+sq = MathMDL.Square()
+sq.length = 3
+
+print("area of this square is",sq.GetSquareArea())
+print("perimeter of this square is",sq.GetSquarePerimeter())
+
+cyl = M.Cylinder()
+cyl.height = 2
+cyl.radius = 1
+print("area of the cylinder is", cyl.GetCylinderSurfaceArea(),"sq units")
+print("volme of the cylinder is", cyl.GetCylinderVolume() ,"cubic units")
+mathlib = M.MathMDL()
+mathlib.numbers = [2,2,2]
+print("vlume s ",mathlib.Add())
+
+tri = M.Triangle()
+tri.height=2
+tri.base = 4
+print("area of triage",tri.GetArea())
+tri.side1=2
+tri.side2 =2
+tri.side3=2
+print("perimeter of the triangle is",tri.GetPerimeter())
+
+temp = M.Temperature()
+temp.celcius = 47
+print("temp in delhi in F is", temp.CelciusToFarenheit(), "degree farenheit.")
+
+mat = M.MathMDL()
+mat.numbers = [1,2,2,3,4]
+print("range is",mat.GetRange())
+print("average is",mat.GetAverage())
+print("mode is",mat.GetMode3())
+'''
+p = M.Pyramid()
+p.length = 5
+p.width = 3
+p.height = 2
+print("de area s",p.GetPyramidArea())
